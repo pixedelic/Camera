65999a567eb1a509abd2c0da5ea8497b4840ea2f
pixedelic <manu@pixedelic.com>
2012-04-29 10:29:25 +0200

    v.1.1.2

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,4 +1,4 @@
-// Camera slideshow v1.1.1 - a jQuery slideshow with many effects, transitions, easy to customize, using canvas and mobile ready, based on jQuery 1.4+
+// Camera slideshow v1.1.2 - a jQuery slideshow with many effects, transitions, easy to customize, using canvas and mobile ready, based on jQuery 1.4+
 // Copyright (c) 2012 by Manuel Masia - www.pixedelic.com
 // Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 ;(function($){$.fn.camera = function(opts, callback) {
@@ -1109,7 +1109,10 @@
 			} else {
 				wT = imgLoaded.naturalWidth;
 				hT = imgLoaded.naturalHeight;
-				$(imgLoaded).attr('width',wT).attr('height',hT).attr('data-alignment',allAlign[slideI]).attr('data-portrait',allPor[slideI]);
+				$(imgLoaded).attr('width',wT);
+				$(imgLoaded).attr('height',hT);
+				$(imgLoaded).attr('data-alignment',allAlign[slideI]);
+				$(imgLoaded).attr('data-portrait',allPor[slideI]);
 				target.find('.cameraSlide_'+slideI).hide().css('visibility','visible');
 				resizeImage();
 				imgLoaded.onload = function() {
