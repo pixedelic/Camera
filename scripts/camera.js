29d7b195bba2b55522c671a66d5033a6e4134c74
pixedelic <manu@pixedelic.com>
2012-06-17 00:46:05 +0200

    v.1.3.3
    Fixed a misprint

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,4 +1,4 @@
-// Camera slideshow v1.3.2 - a jQuery slideshow with many effects, transitions, easy to customize, using canvas and mobile ready, based on jQuery 1.4+
+// Camera slideshow v1.3.3 - a jQuery slideshow with many effects, transitions, easy to customize, using canvas and mobile ready, based on jQuery 1.4+
 // Copyright (c) 2012 by Manuel Masia - www.pixedelic.com
 // Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 ;(function($){$.fn.camera = function(opts, callback) {
